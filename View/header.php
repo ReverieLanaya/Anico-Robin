a63f041833b7da548cba3da36004d1f596aa7c62
Toky Sigogneau <137786950+ReverieLanaya@users.noreply.github.com>
2024-10-11 18:52:32 +0200

    Small update

--- View/header.php
+++ View/header.php
@@ -1,39 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=".//style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Anico Robin</title>
 </head>
 <body>
     
 <header>
+    <div class="header">
 
-    <div class="logo_header">
+        <div class="logo_header">
 
-    <img src="./pics/Anico_robin.png" alt="Logo Anico Robin">
+        <img src="./pics/Anico_robin.png" alt="Logo Anico Robin">
 
-    Anico Robin
-    </div>
-
-    <div class="liens_header">
-        <ul>
-            <li>Rechercher</li>
-            <li>Tendances</li>
-            <li>Ma liste</li>
-
-        </ul>
-    </div>
-
-    <div class="connexion_header">
-        <div>
-            Se  connecter 
+        Anico Robin
         </div>
-        <div>
-            S'inscrire 
+
+        <div class="liens_header">
+            <ul>
+                <li>Rechercher</li>
+                <li>Tendances</li>
+                <li>Ma liste</li>
+
+            </ul>
         </div>
-    </div>
+
+        <div class="connexion_header">
+            <div>
+                Se  connecter 
+            </div>
+            <div>
+                S'inscrire 
+            </div>
+        </div>
+
+</div>
+    
 
 
 
