a2a0b51bf88671f4724213bd814c1024d9890d59
Toky Sigogneau <137786950+ReverieLanaya@users.noreply.github.com>
2024-10-15 16:12:25 +0200

    add gauche part 2

--- View/gauche.php
+++ View/gauche.php
@@ -1,49 +1,65 @@
 <div class="gauche">
-
+        
+    <div class="gauche_p1">
+        
     <a href="index.php" class="liens_gauche">
-        <div class="icone_gauche">
-            <img src="/View/pics/home_icon.png" alt="icone accueil">
-        </div>
+            <div class="icone_gauche">
+                <img src="/View/pics/home_icon.png" alt="icone accueil">
+            </div>
 
-        <span> Accueil</span>
+            <span> Accueil</span>
 
-    </a>
+        </a>
 
-    <a href="index.php" class="liens_gauche">
-        <div class="icone_gauche">
-            <img src="/View/pics/home_icon.png" alt="icone accueil">
-        </div>
+        <a href="index.php" class="liens_gauche">
+            <div class="icone_gauche">
+                <img src="/View/pics/home_icon.png" alt="icone accueil">
+            </div>
 
-        <span> Populaires</span>
+            <span> Populaires</span>
 
-    </a>
+        </a>
 
-    <a href="index.php" class="liens_gauche">
-        <div class="icone_gauche">
-            <img src="/View/pics/home_icon.png" alt="icone accueil">
-        </div>
+        <a href="index.php" class="liens_gauche">
+            <div class="icone_gauche">
+                <img src="/View/pics/home_icon.png" alt="icone accueil">
+            </div>
 
-        <span> Nouveauté</span>
+            <span> Nouveauté</span>
 
-    </a>
+        </a>
 
-    <a href="index.php" class="liens_gauche">
-        <div class="icone_gauche">
-            <img src="/View/pics/home_icon.png" alt="icone accueil">
-        </div>
+        <a href="index.php" class="liens_gauche">
+            <div class="icone_gauche">
+                <img src="/View/pics/home_icon.png" alt="icone accueil">
+            </div>
 
-        <span> Catégories</span>
+            <span> Catégories</span>
 
-    </a>
+        </a>
 
-    <a href="index.php" class="liens_gauche">
-        <div class="icone_gauche">
-            <img src="/View/pics/home_icon.png" alt="icone accueil">
-        </div>
+        <a href="index.php" class="liens_gauche">
+            <div class="icone_gauche">
+                <img src="/View/pics/home_icon.png" alt="icone accueil">
+            </div>
 
-        <span> Voir mon profil</span>
+            <span> Voir mon profil</span>
 
-    </a>
+        </a>
+
+    </div>
+
+    <div class="gauche_p2">
+
+        <a href="index.php" class="liste_gauche">
+                <div class="icone_liste_gauche">
+                    <img src="/View/pics/home_icon.png" alt="icone accueil">
+                </div>
+
+                <span> Ma liste d'anime</span>
+
+            </a>
+    </div>
 
 
 </div>
